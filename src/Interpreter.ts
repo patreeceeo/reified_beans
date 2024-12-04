aa9315cd64cf9e17fda1ff93d8f46d7bb4e075ef
Patrick Canfield <pscale01@gmail.com>
2024-12-04 15:24:14 -0800

    interpret 1 statement/block at a time

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -3,6 +3,7 @@ import {False, True} from "./BooleanObject";
 interface Interpreter {
   run(): boolean;
   step(): boolean;
+  appendCode(code: string): void;
   value: any;
   getStateStack(): Interpreter.State[];
 }
@@ -37,56 +38,3 @@ export function getInterpreter(): IJSInterpreterConstructor {
   return (globalThis as any).Interpreter
 }
 
-export function stepLine(myInterpreter: Interpreter) {
-  const stack = myInterpreter.getStateStack();
-  let more = true;
-  while (more) {
-    more = myInterpreter.step();
-    if (isLine(stack)) {
-      break;
-    }
-  }
-  return more;
-}
-
-let oldStack_ = [] as Interpreter.State[];
-// Is the current stack at the beginning of a new line?
-function isLine(stack: Interpreter.State[]) {
-  var state = stack[stack.length - 1];
-  var node = state.node;
-  var type = node.type;
-  // console.log("state", state);
-
-  if (type !== 'VariableDeclaration' &&
-      !type.endsWith('Statement')) {
-    // Current node is not a statement.
-    return false;
-  }
-
-  if (type === 'BlockStatement') {
-    // Not a 'line' by most definitions.
-    return false;
-  }
-
-  if (type === 'VariableDeclaration' &&
-      stack[stack.length - 2].node.type === 'ForStatement') {
-    // This 'var' is not a line: for (var i = 0; ...)
-    return false;
-  }
-
-  if (oldStack_[oldStack_.length - 1] === state) {
-    // Never repeat the same statement multiple times.
-    // Typically a statement is stepped into and out of.
-    return false;
-  }
-
-  if (oldStack_.indexOf(state) !== -1 && type !== 'ForStatement' &&
-      type !== 'WhileStatement' && type !== 'DoWhileStatement') {
-    // Don't revisit a statement on the stack (e.g. 'if') when exiting.
-    // The exception is loops.
-    return false;
-  }
-
-  oldStack_ = stack.slice();
-  return true;
-}
--- src/components/BlocklyWorkspace.ts
+++ src/components/BlocklyWorkspace.ts
@@ -7,6 +7,12 @@ import {toolbox} from 'src/toolbox';
 import {True} from 'src/BooleanObject';
 import {copyOffsetParentTransform} from '../htmlElement';
 
+interface StatementBlockInfo {
+  code: string;
+  variables: Blockly.VariableModel[];
+  id: string;
+}
+
 export class BlocklyWorkspace extends HTMLElement {
   private injectTarget?: HTMLElement;
   private ws?: Blockly.WorkspaceSvg;
@@ -41,6 +47,42 @@ export class BlocklyWorkspace extends HTMLElement {
     return javascriptGenerator.workspaceToCode(this.ws);
   };
 
+
+  generateStatementBlockInfo(): StatementBlockInfo[] {
+    const workspace = this.ws!;
+    const output = [];
+    javascriptGenerator.init(workspace);
+    const blocks = getStatementBlocksForWorkspace(workspace);
+    for (let i = 0, block; (block = blocks[i]); i++) {
+      // Pass true for opt_thisOnly so we only generate code for this block and not also the attached blocks.
+      let code = javascriptGenerator.blockToCode(block, true);
+      if (Array.isArray(code)) {
+        // Value blocks return tuples of code and operator order.
+        // Top-level blocks don't care about operator order.
+        code = code[0];
+      }
+      if (code) {
+        if (block.outputConnection) {
+          // This block is a naked value.  Ask the language's code generator if
+          // it wants to append a semicolon, or something.
+          code = javascriptGenerator.scrubNakedValue(code);
+          if (javascriptGenerator.STATEMENT_PREFIX && !block.suppressPrefixSuffix) {
+            code = javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block) + code;
+          }
+          if (javascriptGenerator.STATEMENT_SUFFIX && !block.suppressPrefixSuffix) {
+            code = code + javascriptGenerator.injectId(javascriptGenerator.STATEMENT_SUFFIX, block);
+          }
+        }
+        output.push({
+          code,
+          variables: block.getVarModels(),
+          id: block.id
+        });
+      }
+    }
+    return output;
+  }
+
   adjustSize() {
     copyOffsetParentTransform(this, this.injectTarget!);
     Blockly.svgResize(this.ws!);
@@ -49,4 +91,26 @@ export class BlocklyWorkspace extends HTMLElement {
   getAllVariables() {
     return this.ws!.getAllVariables();
   }
+}
+
+/**
+* Given a Blockly block, return the flattened array of statement blocks it contains, assuming that the block itself is a statement block.
+*/
+function getStatementBlocksForBlock(block: Blockly.Block, target: Blockly.Block[] = []): Blockly.Block[] {
+  target.push(block);
+  const children = block.getChildren(true);
+  if(children.length > 0) {
+    const lastChild = children[children.length - 1];
+    if(lastChild.type !== 'math_number') {
+      getStatementBlocksForBlock(lastChild, target);
+    }
+  }
+  return target;
+}
+
+/**
+* Given a Blockly.Workspace, return the flattened array of statement blocks it contains.
+*/
+function getStatementBlocksForWorkspace(ws: Blockly.Workspace): Blockly.Block[] {
+  return ws.getTopBlocks(true).flatMap(b => getStatementBlocksForBlock(b));
 }
