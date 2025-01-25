ea0f8c2cc29db37f174c35743aabef5d713c23cb
Patrick Canfield <pscale01@gmail.com>
2025-01-24 17:55:39 -0800

    auto format

--- src/instructions.ts
+++ src/instructions.ts
@@ -1,5 +1,5 @@
-import {invariant, raise, RangeError, UnknownVMInstruction} from "./errors";
-import {Dict} from "./generics";
+import { invariant, raise, RangeError, UnknownVMInstruction } from "./errors";
+import { Dict } from "./generics";
 
 type Uint2 = 0 | 1 | 2 | 3;
 type Uint3 = Uint2 | 4 | 5 | 6 | 7;
@@ -93,7 +93,6 @@ export enum SpecialSelector {
   Y,
 }
 
-
 export interface Instruction<TArgs extends any[]> {
   readonly type: InstructionType;
   writeWith(writer: InstructionWriter, ...args: TArgs): void;
@@ -102,9 +101,9 @@ export interface Instruction<TArgs extends any[]> {
 }
 
 /**
-* Pop the object from the top of the stack and store in the receiver variable at the given offset
-* @param offset The offset from the start of the receiver variables
-*/
+ * Pop the object from the top of the stack and store in the receiver variable at the given offset
+ * @param offset The offset from the start of the receiver variables
+ */
 export const instPopAndStoreReceiverVar: Instruction<[Uint3]> = {
   type: InstructionType.POP_AND_STORE_RECEIVER_VAR,
 
@@ -119,13 +118,13 @@ export const instPopAndStoreReceiverVar: Instruction<[Uint3]> = {
   readArgs(reader: InstructionReader, target: number[]): [Uint3] {
     target[0] = reader.read() - this.type;
     return target as [Uint3];
-  }
-}
+  },
+};
 
 /**
-* Pop the object from the top of the stack and store in the temporary variable at the given offset
-* @param offset The offset from the start of the temporary variables
-*/
+ * Pop the object from the top of the stack and store in the temporary variable at the given offset
+ * @param offset The offset from the start of the temporary variables
+ */
 export const instPopAndStoreTempVar: Instruction<[Uint3]> = {
   type: InstructionType.POP_AND_STORE_TEMP_VAR,
 
@@ -140,14 +139,14 @@ export const instPopAndStoreTempVar: Instruction<[Uint3]> = {
   readArgs(reader: InstructionReader, target: number[]): [Uint3] {
     target[0] = reader.read() - this.type;
     return target as [Uint3];
-  }
-}
+  },
+};
 
 /**
-* Push a special value onto the eval Stack
-* @param value The special value to Push
-* @see SpecialPushValue
-*/
+ * Push a special value onto the eval Stack
+ * @param value The special value to Push
+ * @see SpecialPushValue
+ */
 export const instPushSpecialVal: Instruction<[SpecialPushValue]> = {
   type: InstructionType.PUSH_SPECIAL_VAL,
 
@@ -162,14 +161,14 @@ export const instPushSpecialVal: Instruction<[SpecialPushValue]> = {
   readArgs(reader: InstructionReader, target: number[]): [SpecialPushValue] {
     target[0] = reader.read() - this.type;
     return target as [SpecialPushValue];
-  }
-}
+  },
+};
 
 /**
-* Return a special Value
-* @param value The special value to return
-* @see SpecialReturnValue
-*/
+ * Return a special Value
+ * @param value The special value to return
+ * @see SpecialReturnValue
+ */
 export const instReturnSpecialVal: Instruction<[SpecialReturnValue]> = {
   type: InstructionType.RETURN_SPECIAL_VAL,
 
@@ -184,15 +183,14 @@ export const instReturnSpecialVal: Instruction<[SpecialReturnValue]> = {
   readArgs(reader: InstructionReader, target: number[]): [SpecialReturnValue] {
     target[0] = reader.read() - this.type;
     return target as [SpecialReturnValue];
-  }
-
-}
+  },
+};
 
 /**
-* Return the object on the top of the return Stack
-* @param value Whether to return from a message or Block
-* @see EvaluationStackType
-*/
+ * Return the object on the top of the return Stack
+ * @param value Whether to return from a message or Block
+ * @see EvaluationStackType
+ */
 export const instReturnStackTopFrom: Instruction<[EvaluationStackType]> = {
   type: InstructionType.RETURN_STACK_TOP_FROM,
 
@@ -207,16 +205,18 @@ export const instReturnStackTopFrom: Instruction<[EvaluationStackType]> = {
   readArgs(reader: InstructionReader, target: number[]): [EvaluationStackType] {
     target[0] = reader.read() - this.type;
     return target as [EvaluationStackType];
-  }
-}
+  },
+};
 
 /**
-* Push an object onto the eval stack
-* @param source The source of the object to push
-* @param offset The offset from the source
-* @see PushSource
-*/
-export const instPush: Instruction<[PushSource, number]> & {readonly bitShift: number} = {
+ * Push an object onto the eval stack
+ * @param source The source of the object to push
+ * @param offset The offset from the source
+ * @see PushSource
+ */
+export const instPush: Instruction<[PushSource, number]> & {
+  readonly bitShift: number;
+} = {
   type: InstructionType.PUSH,
 
   bitShift: 6,
@@ -232,17 +232,23 @@ export const instPush: Instruction<[PushSource, number]> & {readonly bitShift: n
 
   readArgs(reader: InstructionReader, target: number[]): [PushSource, number] {
     reader.read();
-    return unpackBits(reader.read(), this.bitShift!, target as [number, number]);
-  }
-}
+    return unpackBits(
+      reader.read(),
+      this.bitShift!,
+      target as [number, number],
+    );
+  },
+};
 
 /**
-* Store the object on the top of the stack in the indicated location
-* @param target The target of the Store instruction
-* @param offset The offset from the target
-* @see StoreTarget
-*/
-export const instStore: Instruction<[StoreTarget, number]> & {readonly bitShift: number} = {
+ * Store the object on the top of the stack in the indicated location
+ * @param target The target of the Store instruction
+ * @param offset The offset from the target
+ * @see StoreTarget
+ */
+export const instStore: Instruction<[StoreTarget, number]> & {
+  readonly bitShift: number;
+} = {
   type: InstructionType.STORE,
 
   bitShift: 6,
@@ -258,17 +264,23 @@ export const instStore: Instruction<[StoreTarget, number]> & {readonly bitShift:
 
   readArgs(reader: InstructionReader, target: number[]): [StoreTarget, number] {
     reader.read();
-    return unpackBits(reader.read(), this.bitShift!, target as [number, number]);
-  }
-}
+    return unpackBits(
+      reader.read(),
+      this.bitShift!,
+      target as [number, number],
+    );
+  },
+};
 
 /**
-* Pop the object from the top of the stack and store in the indicated location
-* @param target The target of the PopAndStore instruction
-* @param offset The offset from the target
-* @see StoreTarget
-*/
-export const instPopAndStore: Instruction<[StoreTarget, number]> & {readonly bitShift: number} = {
+ * Pop the object from the top of the stack and store in the indicated location
+ * @param target The target of the PopAndStore instruction
+ * @param offset The offset from the target
+ * @see StoreTarget
+ */
+export const instPopAndStore: Instruction<[StoreTarget, number]> & {
+  readonly bitShift: number;
+} = {
   type: InstructionType.POP_AND_STORE,
 
   bitShift: 6,
@@ -284,23 +296,33 @@ export const instPopAndStore: Instruction<[StoreTarget, number]> & {readonly bit
 
   readArgs(reader: InstructionReader, target: number[]): [StoreTarget, number] {
     reader.read();
-    return unpackBits(reader.read(), this.bitShift!, target as [number, number]);
-  }
-}
+    return unpackBits(
+      reader.read(),
+      this.bitShift!,
+      target as [number, number],
+    );
+  },
+};
 
 /**
-* Send a message using a special selector with the given number of arguments.
-* The arguments are taken from the stack, followed by the receiver.
-* @param selector The special selector to Send
-* @param numArgs The number of arguments to the Message
-* @see SpecialSelector
-*/
-export const instSendSpecialSelector: Instruction<[SpecialSelector, number]> & {readonly bitShift: number} = {
+ * Send a message using a special selector with the given number of arguments.
+ * The arguments are taken from the stack, followed by the receiver.
+ * @param selector The special selector to Send
+ * @param numArgs The number of arguments to the Message
+ * @see SpecialSelector
+ */
+export const instSendSpecialSelector: Instruction<[SpecialSelector, number]> & {
+  readonly bitShift: number;
+} = {
   type: InstructionType.SEND_SPECIAL_SELECTOR,
 
   bitShift: 3,
 
-  writeWith(writer: InstructionWriter, selector: SpecialSelector, numArgs: number) {
+  writeWith(
+    writer: InstructionWriter,
+    selector: SpecialSelector,
+    numArgs: number,
+  ) {
     writer.write(this.type);
     writer.write(packBits(selector, numArgs, this.bitShift));
   },
@@ -309,19 +331,24 @@ export const instSendSpecialSelector: Instruction<[SpecialSelector, number]> & {
     return `Send special selector ${SpecialSelector[selector]} with ${numArgs} args`;
   },
 
-  readArgs(reader: InstructionReader, target: number[]): [SpecialSelector, number] {
+  readArgs(
+    reader: InstructionReader,
+    target: number[],
+  ): [SpecialSelector, number] {
     reader.read();
     return unpackBits(reader.read(), this.bitShift, target as [number, number]);
-  }
-}
+  },
+};
 
 /**
-* Send a message using a literal selector with the given number of arguments.
-* The arguments are taken from the stack, followed by the receiver.
-* @param selector The index of the literal selector found in the method's literal array
-* @param numArgs The number of arguments to the message
-*/
-export const instSendLiteralSelectorExt: Instruction<[number, number]> & {readonly bitShift: number} = {
+ * Send a message using a literal selector with the given number of arguments.
+ * The arguments are taken from the stack, followed by the receiver.
+ * @param selector The index of the literal selector found in the method's literal array
+ * @param numArgs The number of arguments to the message
+ */
+export const instSendLiteralSelectorExt: Instruction<[number, number]> & {
+  readonly bitShift: number;
+} = {
   type: InstructionType.SEND_LITERAL_SELECTOR_EXT,
 
   bitShift: 7,
@@ -338,21 +365,27 @@ export const instSendLiteralSelectorExt: Instruction<[number, number]> & {readon
   readArgs(reader: InstructionReader, target: number[]): [number, number] {
     reader.read();
     return unpackBits(reader.read(), this.bitShift, target as [number, number]);
-  }
-}
+  },
+};
 
 /**
-* Send a message to the superclass using a special selector with the given number of arguments.
-* @param selector The special selector to Send
-* @param numArgs The number of arguments to the Message
-* @see SpecialSelector
-*/
-export const instSuperSendSpecialSelector: Instruction<[SpecialSelector, number]> & {readonly bitShift: number} = {
+ * Send a message to the superclass using a special selector with the given number of arguments.
+ * @param selector The special selector to Send
+ * @param numArgs The number of arguments to the Message
+ * @see SpecialSelector
+ */
+export const instSuperSendSpecialSelector: Instruction<
+  [SpecialSelector, number]
+> & { readonly bitShift: number } = {
   type: InstructionType.SUPER_SEND_SPECIAL_SELECTOR,
 
   bitShift: 3,
 
-  writeWith(writer: InstructionWriter, selector: SpecialSelector, numArgs: number) {
+  writeWith(
+    writer: InstructionWriter,
+    selector: SpecialSelector,
+    numArgs: number,
+  ) {
     writer.write(this.type);
     writer.write(packBits(selector, numArgs, this.bitShift));
   },
@@ -361,19 +394,24 @@ export const instSuperSendSpecialSelector: Instruction<[SpecialSelector, number]
     return `Super send special selector ${SpecialSelector[selector]} with ${numArgs} args`;
   },
 
-  readArgs(reader: InstructionReader, target: number[]): [SpecialSelector, number] {
+  readArgs(
+    reader: InstructionReader,
+    target: number[],
+  ): [SpecialSelector, number] {
     reader.read();
     return unpackBits(reader.read(), this.bitShift, target as [number, number]);
-  }
-}
+  },
+};
 
 /**
-* Send a message to the superclass using a literal selector with the given number of arguments.
-* The arguments are taken from the stack, followed by the receiver.
-* @param selector The index of the literal selector found in the method's literal array
-* @param numArgs The number of arguments to the message
-*/
-export const instSuperSendLiteralSelectorExt: Instruction<[number, number]> & {readonly bitShift: 7} = {
+ * Send a message to the superclass using a literal selector with the given number of arguments.
+ * The arguments are taken from the stack, followed by the receiver.
+ * @param selector The index of the literal selector found in the method's literal array
+ * @param numArgs The number of arguments to the message
+ */
+export const instSuperSendLiteralSelectorExt: Instruction<[number, number]> & {
+  readonly bitShift: 7;
+} = {
   type: InstructionType.SUPER_SEND_LITERAL_SELECTOR_EXT,
 
   bitShift: 7,
@@ -390,12 +428,12 @@ export const instSuperSendLiteralSelectorExt: Instruction<[number, number]> & {r
   readArgs(reader: InstructionReader, target: number[]): [number, number] {
     reader.read();
     return unpackBits(reader.read(), this.bitShift, target as [number, number]);
-  }
-}
+  },
+};
 
 /**
-* Pop the object from the top of the Stack
-*/
+ * Pop the object from the top of the Stack
+ */
 export const instPop: Instruction<[]> = {
   type: InstructionType.POP,
 
@@ -410,12 +448,12 @@ export const instPop: Instruction<[]> = {
   readArgs(reader: InstructionReader, target: number[]): [] {
     reader.read();
     return target as [];
-  }
-}
+  },
+};
 
 /**
-* Duplicate the object on the top of the Stack
-*/
+ * Duplicate the object on the top of the Stack
+ */
 export const instDuplicate: Instruction<[]> = {
   type: InstructionType.DUPLICATE,
 
@@ -430,12 +468,12 @@ export const instDuplicate: Instruction<[]> = {
   readArgs(reader: InstructionReader, target: number[]): [] {
     reader.read();
     return target as [];
-  }
-}
+  },
+};
 
 /**
-* Jump the given number of bytes, unconditionally.
-*/
+ * Jump the given number of bytes, unconditionally.
+ */
 export const instJump: Instruction<[number]> = {
   type: InstructionType.JUMP,
 
@@ -452,14 +490,14 @@ export const instJump: Instruction<[number]> = {
     reader.read();
     target[0] = reader.read();
     return target as [number];
-  }
-}
+  },
+};
 
 /**
-* If the object on the top of the Stack is True, pop it and jump the given number of bytes.
-* Otherwise, continue to the next instruction.
-* @param offset The number of bytes to jump, can be negative.
-*/
+ * If the object on the top of the Stack is True, pop it and jump the given number of bytes.
+ * Otherwise, continue to the next instruction.
+ * @param offset The number of bytes to jump, can be negative.
+ */
 export const instPopAndJumpOnTrue: Instruction<[number]> = {
   type: InstructionType.POP_AND_JUMP_ON_TRUE,
 
@@ -476,14 +514,14 @@ export const instPopAndJumpOnTrue: Instruction<[number]> = {
     reader.read();
     target[0] = reader.read();
     return target as [number];
-  }
-}
+  },
+};
 
 /**
-* If the object on the top of the Stack is False, pop it and jump the given number of bytes.
-* Otherwise, continue to the next instruction.
-* @param offset The number of bytes to jump, can be negative.
-*/
+ * If the object on the top of the Stack is False, pop it and jump the given number of bytes.
+ * Otherwise, continue to the next instruction.
+ * @param offset The number of bytes to jump, can be negative.
+ */
 export const instPopAndJumpOnFalse: Instruction<[number]> = {
   type: InstructionType.POP_AND_JUMP_ON_FALSE,
 
@@ -500,8 +538,8 @@ export const instPopAndJumpOnFalse: Instruction<[number]> = {
     reader.read();
     target[0] = reader.read();
     return target as [number];
-  }
-}
+  },
+};
 
 const instructionList: Instruction<any>[] = [
   instPopAndStoreReceiverVar,
@@ -528,15 +566,25 @@ instructionList.sort((a, b) => b.type - a.type);
 
 const instructionCodeCache = Dict<Instruction<any>>();
 
-
 function packBits(num1: number, num2: number, num2Bits: number): number {
-  invariant(num2 < 1 << num2Bits, RangeError, 0, 1 << num2Bits - 1, num2, `a ${num2Bits}-bit number`);
-  return (num1 << num2Bits) | num2
+  invariant(
+    num2 < 1 << num2Bits,
+    RangeError,
+    0,
+    1 << (num2Bits - 1),
+    num2,
+    `a ${num2Bits}-bit number`,
+  );
+  return (num1 << num2Bits) | num2;
 }
 
-function unpackBits(packed: number, num2Bits: number, target: [number, number] = [0, 0]): [number, number] {
+function unpackBits(
+  packed: number,
+  num2Bits: number,
+  target: [number, number] = [0, 0],
+): [number, number] {
   target[0] = packed >> num2Bits;
-  target[1] = packed & (1 << num2Bits) - 1;
+  target[1] = packed & ((1 << num2Bits) - 1);
   return target;
 }
 
@@ -545,11 +593,11 @@ export function peekInstruction(reader: InstructionReader): Instruction<any> {
 
   const cached = instructionCodeCache[code];
 
-  if(cached) {
+  if (cached) {
     return cached;
   }
 
-  for(const inst of instructionList) {
+  for (const inst of instructionList) {
     if (inst.type <= code) {
       instructionCodeCache[code] = inst;
       return inst;
@@ -558,21 +606,34 @@ export function peekInstruction(reader: InstructionReader): Instruction<any> {
   raise(UnknownVMInstruction, code);
 }
 
-const SIGNED_TWO_BYTE_MIN = -1<<15;
-const SIGNED_TWO_BYTE_MAX = (1<<15) - 1;
+const SIGNED_TWO_BYTE_MIN = -1 << 15;
+const SIGNED_TWO_BYTE_MAX = (1 << 15) - 1;
 
 /**
-* My instances write VM instructions to an Int16Array.
-*/
+ * My instances write VM instructions to an Int16Array.
+ */
 export class InstructionWriter {
   private index = 0;
   finished = false;
-  constructor(private target: Int16Array) {
-  }
+  constructor(private target: Int16Array) {}
 
   write(twoBytes: number) {
-    invariant(!this.finished, RangeError, this.index, 0, this.target.length - 1, "an instruction index");
-    invariant(twoBytes >= SIGNED_TWO_BYTE_MIN && twoBytes <= SIGNED_TWO_BYTE_MAX, RangeError, twoBytes, SIGNED_TWO_BYTE_MIN, SIGNED_TWO_BYTE_MAX, "a signed two byte value");
+    invariant(
+      !this.finished,
+      RangeError,
+      this.index,
+      0,
+      this.target.length - 1,
+      "an instruction index",
+    );
+    invariant(
+      twoBytes >= SIGNED_TWO_BYTE_MIN && twoBytes <= SIGNED_TWO_BYTE_MAX,
+      RangeError,
+      twoBytes,
+      SIGNED_TWO_BYTE_MIN,
+      SIGNED_TWO_BYTE_MAX,
+      "a signed two byte value",
+    );
     this.target[this.index++] = twoBytes;
     this.finished = this.index === this.target.length;
   }
@@ -584,16 +645,22 @@ export class InstructionWriter {
 }
 
 /**
-* My instances read VM instructions from a typed array.
-*/
+ * My instances read VM instructions from a typed array.
+ */
 export class InstructionReader {
   private index = 0;
   finished = false;
-  constructor(private source: Int16Array) {
-  }
+  constructor(private source: Int16Array) {}
 
   getAndSkip(n: number): number {
-    invariant(!this.finished, RangeError, this.index, 0, this.source.length - 1, "an instruction index");
+    invariant(
+      !this.finished,
+      RangeError,
+      this.index,
+      0,
+      this.source.length - 1,
+      "an instruction index",
+    );
     const res = this.source[this.index];
     this.index += n;
     this.finished = this.index >= this.source.length;
@@ -601,7 +668,7 @@ export class InstructionReader {
   }
 
   read(): number {
-    return this.getAndSkip(1)
+    return this.getAndSkip(1);
   }
 
   peek(): number {
@@ -613,5 +680,3 @@ export class InstructionReader {
     this.finished = false;
   }
 }
-
-
