4f6b2edcd9e109212e1f02216e6573cc69364eac
Patrick Canfield <pscale01@gmail.com>
2025-02-27 19:53:37 -0800

    False#ifFalse:

--- lib/false.ts
+++ lib/false.ts
@@ -1,11 +1,14 @@
 import { type ClassDescription } from "src/virtual_objects";
+import { dTrue_ifTrue } from "./true";
 
 const dFalse: ClassDescription = {
   name: "False",
   superClass: "Boolean",
   ivars: [],
   classComment: "My instances represent logical inaccuracy.",
-  methodDict: {},
+  methodDict: {
+    "ifFalse:": dTrue_ifTrue,
+  },
 };
 
 export default dFalse;
