dd27e5c44737beadfb09bb960a858a94d25a1786
Patrick Canfield <pscale01@gmail.com>
2025-02-27 19:51:43 -0800

    'at:' is a primitive method

--- lib/array.ts
+++ lib/array.ts
@@ -1,27 +1,11 @@
 import { type ClassDescription } from "src/virtual_objects";
-import type { ClosureDescriptionJs } from "src/closures";
-import {
-  instSendLiteralSelectorExt,
-  type InstructionPointer,
-} from "src/instructions";
-
-const dArray_at: ClosureDescriptionJs = {
-  argCount: 1,
-  tempCount: 0,
-  literals: ["at:"],
-  getInstructions: (pointer: InstructionPointer) => {
-    instSendLiteralSelectorExt.writeWith(pointer, 0, 1);
-  },
-};
 
 const dArray: ClassDescription = {
   name: "Array",
   superClass: "Object",
   ivars: [],
   classComment: "",
-  methodDict: {
-    "at:": dArray_at,
-  },
+  methodDict: {},
 };
 
 export default dArray;
